01f7f20eb2fb0a23e176adcee00f2af5c96d02a1
kelvinlt <CG2024451@365.stucom.com>
2024-10-08 19:22:59 +0200

    main con el trozo principal del codigo a modificar individualmente

---
+++ Python.py
@@ -0,0 +1,11 @@
+numero = 1
+while numero <= 100:
+    if numero % 3 == 0 and numero % 5 == 0:
+        print("CocaCola")
+    elif numero % 3 == 0:
+        print("Coca")
+    elif numero % 5 == 0:
+        print("Cola")
+    else:
+        print(numero)
+    numero += 1
